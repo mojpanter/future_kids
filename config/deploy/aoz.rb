b7d900a73535576108f40cfd9f9438be387bfcf4
Jakub Motycka <jakubonty@centrum.cz>
2020-05-28 11:27:36 +0200

    Revert "remove panter rails deploy in order to remove unicorn"
    This reverts commit e5c42938e2bdffe0f05ee7075ed0f89cafc935b8.

---
+++ config/deploy/aoz.rb
@@ -0,0 +1,2 @@
+server 'future-kids-production.panter.biz', roles: %w[ web app db ]
+set :branch, 'master'
---
+++ config/deploy/phtg.rb
@@ -0,0 +1,2 @@
+server 'futurekids-phtg.panter.biz', roles: %w[ web app db ]
+set :branch, 'master'
